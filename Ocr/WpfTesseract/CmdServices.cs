91a460e2a2fceb7b3eefeba631e9a82c555ec1e1
BSdeployment <142491330+BSdeployment@users.noreply.github.com>
2025-04-22 16:39:34 +0300

    Add files via upload

---
+++ Ocr/WpfTesseract/CmdServices.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfOCR
+{
+    public class CmdServices
+    {
+       
+            public static void Run(string command, out string output, out string error, string directory = null)
+            {
+                using Process process = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "cmd.exe",
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        RedirectStandardInput = true,
+                        Arguments = "/c " + command,
+                        CreateNoWindow = true,
+                        WorkingDirectory = directory ?? string.Empty,
+                        StandardOutputEncoding = Encoding.UTF8, // הגדרת קידוד UTF-8
+                        StandardErrorEncoding = Encoding.UTF8,
+                    }
+                };
+                process.Start();
+                process.WaitForExit();
+                output = process.StandardOutput.ReadToEnd();
+                error = process.StandardError.ReadToEnd();
+            }
+        
+
+    }
+}
---
+++ Ocr/WpfTesseract/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+
+namespace WpfOCR
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                if (files != null && files.Length > 0)
+                {
+                    try
+                    {
+                        // טעינת התמונה
+                        var bitmap = new BitmapImage(new Uri(files[0]));
+                        my_img.Source = bitmap;
+                        txt_url.Text = System.IO.Path.GetFileNameWithoutExtension(files[0]);
+                        Proccess_Image(files[0]);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"שגיאה בטעינת התמונה: {ex.Message}", "שגיאה", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+            }
+        }
+
+        private void Border_DragOver(object sender, DragEventArgs e)
+        {
+            //if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            //{
+            //    e.Effects = DragDropEffects.Copy;
+            //}
+            //else
+            //{
+            //    e.Effects = DragDropEffects.None;
+            //}
+        }
+
+        private void Proccess_Image(string image_path)
+        {
+            CmdServices.Run($"""tesseract "{image_path}" stdout -l eng+heb""",out string output,out string error);
+
+            txt_result.Text = output;
+
+        }
+    }
+}
